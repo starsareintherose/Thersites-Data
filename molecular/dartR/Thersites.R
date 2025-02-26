2c9b0ad3c7c6098b33902a133774e0bd09c697bf
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-02-26 14:58:30 +1100

    polish: R

--- molecular/dartR/Thersites.R
+++ molecular/dartR/Thersites.R
@@ -34,10 +34,45 @@ gl.pcoa.plot(pcoa, gl5, xaxis = 1, yaxis = 3, interactive = FALSE)
 
 pcoa$scores
 
+library(ggplot2)
+
+# change pcoa$scores to data.frame
+pcoa_scores = as.data.frame(pcoa$scores)
+pcoa_scores$id = rownames(pcoa_scores)
+
+# read csv to get pop
+meta = read.csv("meta.ind.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# merge data
+pcoa_data = merge(pcoa_scores, meta, by = "id")
+
+# custom color
+custom_colors = c("darlingtoni" = "#75ff66",
+                   "mitchellae" = "#48b9ff",
+                   "novaehollandiae" = "#ff888a",
+                   "richmondiana" = "#ede100",
+                   "sp_Coolah" = "#b3b3b3",
+                   "sp_MtKaputar" = "#0057b7")
+
+# plot
+ggplot(pcoa_data, aes(x = PC1, y = PC2, color = pop)) +
+  geom_point(size = 3) +
+  scale_color_manual(values = custom_colors) +
+  labs(x = "PC1", y = "PC2", title = "PCoA") +
+  theme_classic()
 
 gl2fasta(
   x = gl5,
   outfile = "gl_output.fasta",
+  outpath = ".",
+  verbose = 2
+)
+
+gl2bpp(
+  x = gl5,
+  outfile = "bpp.txt",
+  method = 2,
+  imap = "Imap.txt",
   outpath = ".",
   verbose = 2
 )
